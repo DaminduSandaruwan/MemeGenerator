31122db0aafeda1673fb64d95707f82f1eceaff4
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-22 00:29:36 +0530

    Adding logo and title

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,38 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 30,),
+            Center(
+              child: Image.asset(
+                "assets/homelogo.png",
+                height: 150,
+                width: 150,
+              ),
+            ),
+            SizedBox(height: 12,),
+            Text(
+              "MEME",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+                fontWeight: FontWeight.w900
+              ),
+            ),
+            Text(
+              "GENERATOR",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+                fontWeight: FontWeight.w900
+              ),
+            ),
+            
+          ],
+        ),
+      ),
     );
   }
 }
