703b282fad6b0557aee6125abf03a0524b6d7d58
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-22 00:21:22 +0530

    Add dependencies to pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
