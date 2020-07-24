88376432c62edbc0df2e28ce1ce375ed357b2be0
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-24 18:25:16 +0530

    Add a shadow

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -71,129 +71,156 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 30,),
-            Center(
-              child: Image.asset(
-                "assets/homelogo.png",
-                height: 75,
-                width: 75,
-              ),
-            ),
-            SizedBox(height: 12,),
-            Text(
-              "MEME",
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 20,
-                fontWeight: FontWeight.w900
-              ),
-            ),
-            Text(
-              "GENERATOR",
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 20,
-                fontWeight: FontWeight.w900
-              ),
-            ),
-            SizedBox(height: 15,),
-            RepaintBoundary(
-              key: globalKey,
-              child: Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  _image != null 
-                  ? Image.file(
-                    _image,
-                    height: 300,
-                    fit: BoxFit.fitHeight,
-                  ) 
-                  : Container(),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 300,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical:12),
-                        child: Text(
-                          headerText.toUpperCase(),
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 26,
-                            fontWeight: FontWeight.w700,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Spacer(),
-                      Container(                        
-                        padding: EdgeInsets.symmetric(vertical:12),
-                        child: Text(
-                          footerText.toUpperCase(),
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 26,
-                            fontWeight: FontWeight.w700,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
+      body: SingleChildScrollView(
+              child: Container(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 30,),
+              Center(
+                child: Image.asset(
+                  "assets/homelogo.png",
+                  height: 75,
+                  width: 75,
                 ),
-                ],
               ),
-            ),
-            SizedBox(height: 20,),
-            imageSelected ? Container(
-              padding: EdgeInsets.symmetric(horizontal:20),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    onChanged: (val){
-                      setState(() {
-                        headerText=val;
-                      });                    
-                    },
-                    decoration: InputDecoration(
-                      hintText: "Header Text",
+              SizedBox(height: 12,),
+              Text(
+                "MEME",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900
+                ),
+              ),
+              Text(
+                "GENERATOR",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900
+                ),
+              ),
+              SizedBox(height: 15,),
+              RepaintBoundary(
+                key: globalKey,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    _image != null 
+                    ? Image.file(
+                      _image,
+                      height: 300,
+                      fit: BoxFit.fitHeight,
+                    ) 
+                    : Container(),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 300,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical:12),
+                          child: Text(
+                            headerText.toUpperCase(),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 26,
+                              fontWeight: FontWeight.w700,
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(2.0,2.0),
+                                  blurRadius: 3.0,
+                                  color: Colors.black87
+                                ),
+                                Shadow(
+                                  offset: Offset(2.0,2.0),
+                                  blurRadius: 8.0,
+                                  color: Colors.black87,
+                                ),
+                              ],
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Spacer(),
+                        Container(                        
+                          padding: EdgeInsets.symmetric(vertical:12),
+                          child: Text(
+                            footerText.toUpperCase(),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 26,
+                              fontWeight: FontWeight.w700,
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(2.0,2.0),
+                                  blurRadius: 3.0,
+                                  color: Colors.black87
+                                ),
+                                Shadow(
+                                  offset: Offset(2.0,2.0),
+                                  blurRadius: 8.0,
+                                  color: Colors.black87,
+                                ),
+                              ],
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 10,),
-                  TextField(
-                    onChanged: (val){
-                      setState(() {
-                        footerText=val;                      
-                      });
-                    },
-                    decoration: InputDecoration(
-                      hintText: "Footer Text",
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  RaisedButton(
-                    onPressed: (){
-                      takeScreenshot();
-                    },
-                    child: Text(
-                      "Save",
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ) 
-            : Container(
-              child: Center(
-                child: Text("Select an image to get started"),
+              SizedBox(height: 20,),
+              imageSelected ? Container(
+                padding: EdgeInsets.symmetric(horizontal:20),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      onChanged: (val){
+                        setState(() {
+                          headerText=val;
+                        });                    
+                      },
+                      decoration: InputDecoration(
+                        hintText: "Header Text",
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    TextField(
+                      onChanged: (val){
+                        setState(() {
+                          footerText=val;                      
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: "Footer Text",
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    RaisedButton(
+                      onPressed: (){
+                        takeScreenshot();
+                      },
+                      child: Text(
+                        "Save",
+                      ),
+                    ),
+                  ],
+                ),
+              ) 
+              : Container(
+                child: Center(
+                  child: Text("Select an image to get started"),
+                ),
               ),
-            ),
-            _imageFile != null ? Image.file(_imageFile) : Container(),
-          ],
+              SizedBox(height: 20,),
+              _imageFile != null ? Image.file(_imageFile) : Container(),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
